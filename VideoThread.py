c9acdcc0b7171cebb39923037a10fe9f809db282
SGCMarkus <markusornik@gmail.com>
2023-08-01 17:59:12 +0200

    VideoThread: add timestamps to the video

--- VideoThread.py
+++ VideoThread.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import pyqtSignal, QThread
 import numpy as np
 import cv2
-import time
+from astropy.time import Time
 
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
@@ -18,20 +18,48 @@ class VideoThread(QThread):
         self.setDifferentialTimeForFPS()
 
     def setDifferentialTimeForFPS(self):
-        self.differntialTime = 1000/self.targetFPS/1000
+        self.differntialTime = 1000/self.targetFPS/100000000
 
+    def putTimestampsOnImage(self, cv_img, when):
+        mjd = int(when.mjd)
+        tmp = when.iso.split('.')[0].split(' ')
+        
+        date = tmp[0]; time = tmp[1]
+
+        cv_img = self.putLineOnImage(cv_img, date, 0)
+        cv_img = self.putLineOnImage(cv_img, "UT " + time, 1)
+        cv_img = self.putLineOnImage(cv_img, "MJD " + str(mjd), 2)
+        return cv_img
+
+    def putLineOnImage(self, cv_img, text, line):
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_scale = 2; margin = 5; thickness = 2
+        color = (255, 255, 255)
+
+        size = cv2.getTextSize(text, font, font_scale, thickness)
+
+        textWidth = size[0][0]
+        textHeight = size[0][1]
+        lineHeight = textHeight + size[1] + margin
+
+        x = cv_img.shape[1] - margin - textWidth
+        y = margin + size[0][1] + line * lineHeight
+
+        return cv2.putText(cv_img, text, (x, y), font, font_scale, color, thickness)
 
     def run(self):
         # capture from web cam
         cap = cv2.VideoCapture(self.camPort)
         self._run_flag = True
-        lastTime = time.time()
+        lastTime = Time.now()
         while self._run_flag:
             ret, cv_img = cap.read()
-            nowTime = time.time()
+            nowTime = Time.now()
             if ret and (nowTime - lastTime) > self.differntialTime:
+                cv_img = self.putTimestampsOnImage(cv_img, nowTime)
+
                 self.change_pixmap_signal.emit(cv_img)
-                lastTime = time.time()
+                lastTime = Time.now()
         # shut down capture system
         cap.release()
 
