2a4dd92808862bb3bd92cd59b2fe4ea284391750
SGCMarkus <markusornik@gmail.com>
2023-09-14 14:58:27 +0200

    WeatherDataThread: use config file

--- WeatherDataThread.py
+++ WeatherDataThread.py
@@ -5,6 +5,7 @@ from astropy.time import Time
 import astropy.units as u
 import win32com.client as com
 from typing import Final
+import os
 
 class WeatherDataThread(QThread):
     updateWeatherDataSignal = pyqtSignal(object)
@@ -26,8 +27,9 @@ class WeatherDataThread(QThread):
     RAIN_COND_STR: Final[str] = "RainCond"
     DAY_COND_STR: Final[str] = "DayCond"
 
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
 
     def getCloudConditionString(self, condition: int):
         match condition:
@@ -113,27 +115,60 @@ class WeatherDataThread(QThread):
 
     def run(self):
         self._run_flag = True
-        self.cloud = com.gencache.EnsureDispatch("ClarityII.CloudSensorII")
-        lastTime = Time.now()
-        while self._run_flag:
-            if(self.cloud is not None):
-                data = {WeatherDataThread.REL_SKY_TEMP_STR: self.cloud.RelSkyT,
-                        WeatherDataThread.AMBIENT_TEMP_STR: self.cloud.AmbientT,
-                        WeatherDataThread.SENSOR_TEMP_STR: self.cloud.SensorT,
-                        WeatherDataThread.WIND_STR: self.cloud.Wind,
-                        WeatherDataThread.HUMIDITY_STR: self.cloud.HumidityPercent,
-                        WeatherDataThread.DEW_POINT_STR: self.cloud.DewPointT,
-                        WeatherDataThread.DAYLIGHT_STR: self.cloud.DayLightV,
-                        WeatherDataThread.RAIN_F_STR: self.cloud.RainF,
-                        WeatherDataThread.WET_F_STR: self.cloud.WetF,
-                        WeatherDataThread.HEATER_STR: self.cloud.HeaterPercent,
-                        WeatherDataThread.LAST_TIME_OK_STR: self.cloud.SecondsSinceGoodData,
-                        WeatherDataThread.CLOUD_COND_STR: self.cloud.CloudCondition,
-                        WeatherDataThread.WIND_COND_STR: self.cloud.WindCondition,
-                        WeatherDataThread.RAIN_COND_STR: self.cloud.RainCondition,
-                        WeatherDataThread.DAY_COND_STR: self.cloud.DayCondition}
-                self.updateWeatherDataSignal.emit(data)
-            t.sleep(1)
+        if(not os.path.isfile(self.config["BOLTWOOD"]["BoltwoodClarityIIInstallPath"])):
+            print("Clarity II not found! Install it to get Boltwood data")
+        else:
+            self.cloud = com.gencache.EnsureDispatch("ClarityII.CloudSensorII")
+            CloudLevelStrEnabled = self.config["BOLTWOOD"].getboolean("CloudLevelStrEnabled")
+            WindLevelStrEnabled = self.config["BOLTWOOD"].getboolean("WindLevelStrEnabled")
+            HumidityLevelStrEnabled = self.config["BOLTWOOD"].getboolean("HumidityLevelStrEnabled")
+            DaylightLevelStrEnabled = self.config["BOLTWOOD"].getboolean("DaylightLevelStrEnabled")
+            RainIndicatorEnabled = self.config["BOLTWOOD"].getboolean("RainIndicatorEnabled")
+            WetIndicatorEnabled = self.config["BOLTWOOD"].getboolean("WetIndicatorEnabled")
+            SkyAmbTempEnabled = self.config["BOLTWOOD"].getboolean("SkyAmbTempEnabled")
+            AmbientTempEnabled = self.config["BOLTWOOD"].getboolean("AmbientTempEnabled")
+            SensorTempEnabled = self.config["BOLTWOOD"].getboolean("SensorTempEnabled")
+            RainHeaterEnabled = self.config["BOLTWOOD"].getboolean("RainHeaterEnabled")
+            WindSpeedEnabled = self.config["BOLTWOOD"].getboolean("WindSpeedEnabled")
+            HumidityEnabled = self.config["BOLTWOOD"].getboolean("HumidityEnabled")
+            DewPointEnabled = self.config["BOLTWOOD"].getboolean("DewPointEnabled")
+            DaylightEnabled = self.config["BOLTWOOD"].getboolean("DaylightEnabled")
+            lastTime = Time.now()
+            while self._run_flag:
+                if(self.cloud is not None):
+                    data = {}
+                    if(SkyAmbTempEnabled):
+                        data[WeatherDataThread.REL_SKY_TEMP_STR] = self.cloud.RelSkyT
+                    if(AmbientTempEnabled):
+                        data[WeatherDataThread.AMBIENT_TEMP_STR] = self.cloud.AmbientT
+                    if(SensorTempEnabled):
+                        data[WeatherDataThread.SENSOR_TEMP_STR] = self.cloud.SensorT
+                    if(WindSpeedEnabled):
+                        data[WeatherDataThread.WIND_STR] = self.cloud.Wind
+                    if(HumidityEnabled):
+                        data[WeatherDataThread.HUMIDITY_STR] = self.cloud.HumidityPercent
+                    if(DewPointEnabled):
+                        data[WeatherDataThread.DEW_POINT_STR] = self.cloud.DewPointT
+                    if(DaylightEnabled):
+                        data[WeatherDataThread.DAYLIGHT_STR] = self.cloud.DayLightV
+                    if(RainIndicatorEnabled):
+                        data[WeatherDataThread.RAIN_F_STR] = self.cloud.RainF
+                    if(WetIndicatorEnabled):
+                        data[WeatherDataThread.WET_F_STR] = self.cloud.WetF
+                    if(RainHeaterEnabled):
+                        data[WeatherDataThread.HEATER_STR] = self.cloud.HeaterPercent
+                    if(CloudLevelStrEnabled):
+                         data[WeatherDataThread.CLOUD_COND_STR] = self.cloud.CloudCondition
+                    if(WindLevelStrEnabled):
+                        data[WeatherDataThread.WIND_COND_STR] = self.cloud.WindCondition
+                    if(HumidityLevelStrEnabled):
+                        data[WeatherDataThread.RAIN_COND_STR] = self.cloud.RainCondition
+                    if(DaylightLevelStrEnabled):
+                        data[WeatherDataThread.DAY_COND_STR] = self.cloud.DayCondition
+                
+                    data[WeatherDataThread.LAST_TIME_OK_STR] = self.cloud.SecondsSinceGoodData
+                    self.updateWeatherDataSignal.emit(data)
+                t.sleep(1)
 
     def stop(self):
         """Sets run flag to False and waits for thread to finish"""
